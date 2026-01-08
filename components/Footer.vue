c67615fcbf61476dc72179049029c004c82c23a0
Mileer leon <mileerdubanleon@gmail.com>
2026-01-08 15:12:58 -0500

    change: footer text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="w-full px-6 py-6 text-center text-sm text-gray-600 border-t border-gray-200">
-    © {{ new Date().getFullYear() }} Spencer Gabor. All rights reserved.
+    © {{ new Date().getFullYear() }} Mileer león. 3246037753 All rights reserved.
   </footer>
 </template>
