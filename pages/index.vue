5f983ee0ad6d3666dbd00d041a6f7cfa57eb1162
Mileer leon <mileerdubanleon@gmail.com>
2026-01-09 15:07:47 -0500

    fix: red social btn and scroll top

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="min-h-screen flex flex-col">
+    <SocialNetwork />
+    <SocialNetworkPhone />
     <Header />
     <ProjectFloating />
     <ProjectFloatingVercel />
     <ServicesSection />
     <ContactForm />
+    <ScrollTop />
     <Footer />
     <ThemeToggle />
   </div>
 </template>
 
 <script setup>
+  import SocialNetwork from '@/components/SocialNetwork.vue'
+  import SocialNetworkPhone from '@/components/SocialNetworkPhone.vue'
   import Header from '@/components/Header.vue'
   import ProjectFloating from '@/components/ProjectFloating.vue'
   import ProjectFloatingVercel from '@/components/ProjectFloatingVercel.vue'
   import ServicesSection from '@/components/services/ServicesSection.vue'
   import ContactForm from '@/components/ContactForm.vue'
+  import ScrollTop from '@/components/ScrollTop.vue'
   import Footer from '@/components/Footer.vue'
   import ThemeToggle from '@/components/ThemeToggle.vue'
 </script>
